6202f7991d638dd6ff6138c1a5247268b6b25251
jingyao-d <jyzhang256@gmail.com>
2024-10-24 17:16:36 -0700

    some random midi stuff

--- Assets/Scripts/songManager.cs
+++ Assets/Scripts/songManager.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
+using UnityEngine.Networking;
+using System.IO;
+using System;
 
 public class songManager : MonoBehaviour
 {
+    public static songManager Instance;
+    public AudioSource audioSource;
+    public float songDelay; //seconds
+    public double MoE;
+    public int inputDelayms;
+    
+
+    
+    public string fileLocation;
+    public float noteTime;
+
+    public static MidiFile midiFile;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instance = this;
+        ReadFromFile();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void ReadFromFile()
+    {
+        midiFile = MidiFile.Read(Application.streamingAssetsPath + "/" + fileLocation);
+        GetDataFromMidi();
+    }
+
+    public void GetDataFromMidi()
+    {
+        var notes = midiFile.GetNotes();
+        var array = new Melanchall.DryWetMidi.Interaction.Note[notes.Count];
+        notes.CopyTo(array, 0);
+
+        Invoke(nameof(StartSong), songDelay);
+    }
+
+    public void StartSong()
+    {
+        audioSource.Play();
+    }
+
+    public static double GetAudioSourceTime()
+    {
+        return (double) Instance.audioSource.timeSamples / Instance.audioSource.clip.frequency;
     }
 }
